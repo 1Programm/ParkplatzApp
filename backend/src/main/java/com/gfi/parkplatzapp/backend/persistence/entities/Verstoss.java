18083056bc8a88e60f325e520815843b05d286e0
lgall <lukas.gall@ssi-schaefer.com>
2023-06-07 11:10:29 +0200

    Update Datenbank Entity

---
+++ backend/src/main/java/com/gfi/parkplatzapp/backend/persistence/entities/Verstoss.java
@@ -0,0 +1,27 @@
+package com.gfi.parkplatzapp.backend.persistence.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Verstoss {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long meldeID;
+
+    private Date datum;
+    private String bemerkung;
+}
