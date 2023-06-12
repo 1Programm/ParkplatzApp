cc8068e9a5a78ac68a6c84141c3d515c8485e882
kathari00 <katharina.eiserfey1@gmail.com>
2023-06-12 11:24:07 +0200

    Entities in frontend and profile service

---
+++ frontend/src/app/facade/Buchung.ts
@@ -0,0 +1,10 @@
+import { Mitarbeiter } from "./Mitarbeiter"
+import { Parkplatz } from "./Parkplatz"
+
+export interface Buchung {
+    buchungID: number
+    datum: Date
+    tagespreis: number
+    mitarbeiter: Mitarbeiter
+    parkplatz: Parkplatz
+}
