1a16a880e63328c515a16763f774e3b4c13e9bdf
Omar <itodigital@hotmail.com>
2023-06-12 11:15:42 +0200

    - ParkflaecheAuswahl-DTO erstellt

---
+++ frontend/src/app/dtos/parkflaeche-auswahl.dto.spec.ts
@@ -0,0 +1,7 @@
+import { ParkflaecheAuswahlDto } from './parkflaeche-auswahl.dto';
+
+describe('ParkflaecheAuswahlDto', () => {
+  it('should create an instance', () => {
+    expect(new ParkflaecheAuswahlDto()).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/dtos/parkflaeche-auswahl.dto.ts
@@ -0,0 +1,6 @@
+export class ParkflaecheAuswahlDto {
+    public parkhausId?: number;
+    public parkhausBezeichnung?: string;
+    public parkflaecheId?: number;
+    public parkflaecheBezeichnung?: string;
+}
