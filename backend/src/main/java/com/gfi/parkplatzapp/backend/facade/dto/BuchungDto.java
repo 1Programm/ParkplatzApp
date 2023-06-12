5491cd2874d410101c8682de7e15f88a524546d1
kathari00 <katharina.eiserfey1@gmail.com>
2023-06-12 12:41:33 +0200

    Dto update

--- backend/src/main/java/com/gfi/parkplatzapp/backend/facade/dto/BuchungDto.java
+++ backend/src/main/java/com/gfi/parkplatzapp/backend/facade/dto/BuchungDto.java
@@ -1,2 +1,28 @@
-package com.gfi.parkplatzapp.backend.facade.dto;public class BuchungDto {
+package com.gfi.parkplatzapp.backend.facade.dto;
+
+import com.gfi.parkplatzapp.backend.persistence.entities.Kennzeichen;
+import com.gfi.parkplatzapp.backend.persistence.entities.Mitarbeiter;
+import com.gfi.parkplatzapp.backend.persistence.entities.Parkplatz;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BuchungDto {
+    private Long buchungID;
+
+    private Date datum;
+    private Double tagespreis;
+    private String parkplatzKennung;
+    private Kennzeichen kennzeichen;
 }
+
+
