0f8c3b701a920c851aa0d84d168789d12b278162
itodigital <itodigital@hotmail.com>
2023-06-13 13:05:18 +0200

    - BuchungService erstellt

--- frontend/src/app/services/buchung.service.ts
+++ frontend/src/app/services/buchung.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, retry, throwError } from 'rxjs';
 import { ParkflaecheAuswahlDto } from '../dtos/parkflaeche-auswahl.dto';
 import { environment } from 'src/environments/environment';
 
@@ -12,6 +12,25 @@ export class BuchungService {
   constructor(private http: HttpClient){}
 
   public getParkanlagen(): Observable<ParkflaecheAuswahlDto[]> {
-    return this.http.get<ParkflaecheAuswahlDto[]>(`${environment.apiServerUrl}/data/parkanlagen`);
+    return this.http.get<ParkflaecheAuswahlDto[]>(`${environment.apiServerUrl}/buchung/parkanlagen`).pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
   }
+
+
+  // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }
