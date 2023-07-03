a2ee2b09564b6f69052c75ad0bb82f5f2cf2b756
Programm0 <1Pro2gra3mm45@web.de>
2023-07-03 11:08:01 +0200

    Fixing test setup

--- backend/src/Test/Java/com/gfi/parkplatzapp/backend/service/MitarbeiterServiceTest.java
+++ backend/src/Test/Java/com/gfi/parkplatzapp/backend/service/MitarbeiterServiceTest.java
@@ -1,39 +1,50 @@
 package com.gfi.parkplatzapp.backend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gfi.parkplatzapp.backend.Application;
 import com.gfi.parkplatzapp.backend.persistence.entities.Mitarbeiter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.RequestEntity.post;
 
-@WebMvcTest(controllers = MitarbeiterService.class)
+//@WebMvcTest(controllers = MitarbeiterService.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@ActiveProfiles("test")
 class MitarbeiterServiceTest {
+
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
+//    @MockBean
+    @Autowired
     private MitarbeiterService mitarbeiterService;
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
     public void getMitarbeiter_Equals_Vorname () throws Exception {
-        assertEquals(mitarbeiterService.getMitarbeiter(1L).getVorname(), "Max");
+        Mitarbeiter mitarbeiter = mitarbeiterService.getMitarbeiter(1L);
+        assertNotNull(mitarbeiter);
+        assertEquals(mitarbeiter.getVorname(), "Max");
     }
 
     @Test
---
+++ backend/src/main/resources/test-data.sql
@@ -0,0 +1,141 @@
+
+
+
+-- Mitarbeiter
+INSERT INTO Mitarbeiter (mitarbeiterID, mail, vorname, nachname)
+VALUES(1, 'max@example.com', 'Maxi', 'Mustermann');
+
+INSERT INTO Mitarbeiter (mitarbeiterID, mail, vorname, nachname)
+VALUES(2, 'sabine@example.com', 'Sabine', 'Schmidt');
+
+INSERT INTO Mitarbeiter (mitarbeiterID, mail, vorname, nachname)
+VALUES(3, 'thomas@example.com', 'Thomas', 'Müller');
+
+
+
+-- Parkflaeche
+INSERT INTO Parkflaeche (parkflaecheID, bezeichnung)
+VALUES(1, 'Fläche A');
+
+INSERT INTO Parkflaeche (parkflaecheID, bezeichnung)
+VALUES(2, 'Fläche B');
+
+-- Parkhaus
+INSERT INTO Parkhaus (parkhausID, bezeichnung, hausnummer, ort, plz, strasse)
+VALUES(1, 'Parkhaus 1', '1', 'Berlin', '12345', 'Hauptstraße');
+
+INSERT INTO Parkhaus (parkhausID, bezeichnung, hausnummer, ort, plz, strasse)
+VALUES(2, 'Parkhaus 2', '2', 'München', '67890', 'Nebenstraße');
+
+-- Parkplatztyp
+INSERT INTO Parkplatztyp (parkplatztypID, beschreibung, bezeichnung)
+VALUES(1, 'Standard-Parkplatz', 'Standard');
+
+INSERT INTO Parkplatztyp (parkplatztypID, beschreibung, bezeichnung)
+VALUES(2, 'Parkplatz für Behinderte', 'Behindertenparkplatz');
+
+INSERT INTO Parkplatztyp (parkplatztypID, beschreibung, bezeichnung)
+VALUES(3, 'VIP-Parkplatz', 'VIP');
+
+-- Preiskategorie
+INSERT INTO Preiskategorie (kategorieID, bezeichnung, preis)
+VALUES(1, 'Normal', 10.00);
+
+INSERT INTO Preiskategorie (kategorieID, bezeichnung, preis)
+VALUES(2, 'Premium', 15.00);
+
+-- Verstoss
+INSERT INTO Verstoss (meldeID, bemerkung, datum)
+VALUES(1, 'Falschparken', '2023-06-01');
+
+INSERT INTO Verstoss (meldeID, bemerkung, datum)
+VALUES(2, 'Geschwindigkeitsüberschreitung', '2023-06-02');
+
+INSERT INTO Verstoss (meldeID, bemerkung, datum)
+VALUES(3, 'Parken auf Behindertenparkplatz', '2023-06-03');
+
+--Kennzeichen
+INSERT INTO Kennzeichen (kennzeichenID, kennzeichen)
+VALUES(1, 'DO-JB1999');
+
+INSERT INTO Kennzeichen (kennzeichenID, kennzeichen)
+VALUES(2, 'DO-JB1998');
+
+INSERT INTO Kennzeichen (kennzeichenID, kennzeichen)
+VALUES(3, 'DO-JB1997');
+
+INSERT INTO Kennzeichen (kennzeichenID, kennzeichen)
+VALUES(4, 'DO-KE1200');
+
+-- Parkplatz
+INSERT INTO Parkplatz (nummer, x_Koordinate, y_Koordinate, parkplatztyp_parkplatztypID, preiskategorie_kategorieID)
+VALUES('P1', 185, 132, 1, 1);
+
+INSERT INTO Parkplatz (nummer, x_Koordinate, y_Koordinate, parkplatztyp_parkplatztypID, preiskategorie_kategorieID)
+VALUES('P2', 135, 132, 2, 2);
+
+INSERT INTO Parkplatz (nummer, x_Koordinate, y_Koordinate, parkplatztyp_parkplatztypID, preiskategorie_kategorieID)
+VALUES('P3', 2, 300, 1, 1);
+
+INSERT INTO Parkplatz ( nummer, x_Koordinate, y_Koordinate, parkplatztyp_parkplatztypID, preiskategorie_kategorieID)
+VALUES('P4', 1, 10, 3, 2);
+
+-- Buchung
+INSERT INTO Buchung (buchungID, datum, tagespreis, mitarbeiter_mitarbeiterID, parkplatz_parkplatzID, kennzeichen_kennzeichenID)
+VALUES(1, '2023-06-01', 10.50, 1, 1, 1);
+
+INSERT INTO Buchung (buchungID, datum, tagespreis, mitarbeiter_mitarbeiterID, parkplatz_parkplatzID, kennzeichen_kennzeichenID)
+VALUES(2, '2023-06-02', 15.00, 2, 3, 3);
+
+INSERT INTO Buchung (buchungID, datum, tagespreis, mitarbeiter_mitarbeiterID, parkplatz_parkplatzID, kennzeichen_kennzeichenID)
+VALUES(5, '2023-08-30', 15.00, 1, 3, 1);
+
+INSERT INTO Buchung (buchungID, datum, tagespreis, mitarbeiter_mitarbeiterID, parkplatz_parkplatzID, kennzeichen_kennzeichenID)
+VALUES(3, '2023-06-03', 12.75, 3, 2, 4);
+
+INSERT INTO Buchung (buchungID, datum, tagespreis, mitarbeiter_mitarbeiterID, parkplatz_parkplatzID, kennzeichen_kennzeichenID)
+VALUES(4, '2023-06-04', 9.99, 1, 4, 2);
+
+-- Mitarbeiter_verstoss_list
+INSERT INTO Mitarbeiter_verstoss_list (mitarbeiter_mitarbeiterID, verstoss_list_meldeID)
+VALUES(1, 1);
+
+INSERT INTO Mitarbeiter_verstoss_list (mitarbeiter_mitarbeiterID, verstoss_list_meldeID)
+VALUES(2, 2);
+
+INSERT INTO Mitarbeiter_verstoss_list (mitarbeiter_mitarbeiterID, verstoss_list_meldeID)
+VALUES(2, 3);
+
+--Mitarbeiter_kennzeichen_list
+INSERT INTO Mitarbeiter_kennzeichen_list(mitarbeiter_mitarbeiterID, kennzeichen_list_kennzeichenID)
+VALUES(1,1);
+
+INSERT INTO Mitarbeiter_kennzeichen_list(mitarbeiter_mitarbeiterID, kennzeichen_list_kennzeichenID)
+VALUES(1,2);
+
+INSERT INTO Mitarbeiter_kennzeichen_list(mitarbeiter_mitarbeiterID, kennzeichen_list_kennzeichenID)
+VALUES(2,3);
+
+INSERT INTO Mitarbeiter_kennzeichen_list(mitarbeiter_mitarbeiterID, kennzeichen_list_kennzeichenID)
+VALUES(3,4);
+
+-- parkflaeche_parkplatz_list
+INSERT INTO parkflaeche_parkplatz_list (parkflaeche_parkflaecheID, parkplatz_list_parkplatzID)
+VALUES(1, 1);
+
+INSERT INTO parkflaeche_parkplatz_list (parkflaeche_parkflaecheID, parkplatz_list_parkplatzID)
+VALUES(1, 2);
+
+INSERT INTO parkflaeche_parkplatz_list (parkflaeche_parkflaecheID, parkplatz_list_parkplatzID)
+VALUES(2, 3);
+
+INSERT INTO parkflaeche_parkplatz_list (parkflaeche_parkflaecheID, parkplatz_list_parkplatzID)
+VALUES(2, 4);
+
+-- parkhaus_parkflaeche_list
+INSERT INTO parkhaus_parkflaeche_list (parkhaus_parkhausID, parkflaeche_list_parkflaecheID)
+VALUES(1, 1);
+
+INSERT INTO parkhaus_parkflaeche_list (parkhaus_parkhausID, parkflaeche_list_parkflaecheID)
+VALUES(1, 2);
+
