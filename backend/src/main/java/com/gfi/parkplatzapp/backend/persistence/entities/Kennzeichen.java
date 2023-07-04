ab61392e1e70160370fc96b417d893e47e03c1cb
Programm0 <1Pro2gra3mm45@web.de>
2023-07-04 15:04:37 +0200

    Fix Date in Entities and cleanup

--- backend/src/main/java/com/gfi/parkplatzapp/backend/persistence/entities/Kennzeichen.java
+++ backend/src/main/java/com/gfi/parkplatzapp/backend/persistence/entities/Kennzeichen.java
@@ -1,9 +1,10 @@
 package com.gfi.parkplatzapp.backend.persistence.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-
-import java.util.Objects;
 
 
 @Entity
