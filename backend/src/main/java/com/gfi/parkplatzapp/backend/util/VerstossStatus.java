8d4fa4b4a1d53b6aab230f73b1db7a5bdd518055
Omar <itodigital@hotmail.com>
2023-07-06 13:50:40 +0200

    - parseError gefixt

--- backend/src/main/java/com/gfi/parkplatzapp/backend/util/VerstossStatus.java
+++ backend/src/main/java/com/gfi/parkplatzapp/backend/util/VerstossStatus.java
@@ -1,10 +1,17 @@
 package com.gfi.parkplatzapp.backend.util;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum VerstossStatus {
-    IN_BEARBEITUNG("In Bearbeitung"),
+    IN_BEARBEITUNG("In_Bearbeitung"),
     ABGESCHLOSSEN("Abgeschlossen");
 
     private String enumValue;
+
+    @JsonCreator
+    public static VerstossStatus parseFromString(String value) {
+        return VerstossStatus.valueOf(value.toUpperCase());
+    }
 
     private VerstossStatus(String enumValue) {
         this.enumValue = enumValue;
